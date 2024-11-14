c63455b276c5a9e77d5eeb6ad7abde640d74b543
honoreRemi <massackhonoreremi@mail.com>
2024-11-14 16:12:48 +0100

    Toutes les combinaisons voulues dans 10

--- combinaisonDeQUatreDansDix.py
+++ combinaisonDeQUatreDansDix.py
@@ -4,12 +4,17 @@ import itertools
 
 # Nombre total d'éléments
 n = 10
-# Nombre d'éléments à choisir
-r = 4
 
-# Générer toutes les combinaisons
-combinations = list(itertools.combinations(range(n), r))
+# Demander à l'utilisateur combien d'éléments il souhaite combiner
+r = int(input("Combien d'éléments voulez-vous combiner (max 10) ? "))
 
-# Afficher les combinaisons
-for combo in combinations:
-    print(combo)
+if r > n or r < 1:
+    print("Veuillez entrer un nombre valide entre 1 et 10.")
+else:
+    # Générer toutes les combinaisons
+    combinations = list(itertools.combinations(range(n), r))
+
+    # Afficher les combinaisons
+    print(f"Toutes les combinaisons de {r} éléments parmi {n} :")
+    for combo in combinations:
+        print(combo)
